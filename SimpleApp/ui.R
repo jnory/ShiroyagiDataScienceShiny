ee3d60e10973e548625c3d3d80070e642daf3034
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2016-12-06 11:13:44 +0900

    Simpler.

--- SimpleApp/ui.R
+++ SimpleApp/ui.R
@@ -1,15 +1,7 @@
 shinyUI(
   fluidPage(
     titlePanel("入力をそのまま出力"),
-    sidebarLayout(
-      sidebarPanel(
-        textInput(
-          "user_input", "入力"
-        )
-      ),
-      mainPanel(
-        textOutput("display_text")
-      )
-    )
+    textInput("user_input", "入力"),
+    textOutput("display_text")
   )
 )
