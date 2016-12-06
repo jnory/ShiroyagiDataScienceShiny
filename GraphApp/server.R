122ff05271b50282ff8eb62538c924483ee8ec0b
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2016-12-06 13:31:52 +0900

    Answer for enshu2.

--- GraphApp/server.R
+++ GraphApp/server.R
@@ -5,10 +5,23 @@ shinyServer(
     output$iris_plot <- renderPlot({
       gp <- ggplot(iris, aes(Petal.Length, Petal.Width))
       gp <- gp + theme(legend.justification=c(0, 1), legend.position=c(0, 1))
+      
+      x_data <- switch(input$xaxis,
+                       "Sepal.Length" = iris$Sepal.Length,
+                       "Sepal.Width" = iris$Sepal.Width,
+                       "Petal.Length" = iris$Petal.Length,
+                       "Petal.Width" = iris$Petal.Width
+      )
+      y_data <- switch(input$yaxis,
+                       "Sepal.Length" = iris$Sepal.Length,
+                       "Sepal.Width" = iris$Sepal.Width,
+                       "Petal.Length" = iris$Petal.Length,
+                       "Petal.Width" = iris$Petal.Width
+      )
       if(input$coloring){
-        gp + geom_point(aes(color=Species))
+        gp + geom_point(aes(x_data, y_data, color=Species))
       }else{
-        gp + geom_point()
+        gp + geom_point(aes(x_data, y_data))
       }
     })
   }
--- GraphApp/ui.R
+++ GraphApp/ui.R
@@ -3,7 +3,15 @@ shinyUI(
     titlePanel("Iris Plot"),
     sidebarLayout(
       sidebarPanel(
-        checkboxInput("coloring", "Coloring")
+        checkboxInput("coloring", "Coloring"),
+        selectInput("xaxis", "X-axis", 
+                    choices=c(
+                      "Sepal.Length", "Sepal.Width",
+                      "Petal.Length", "Petal.Width")),
+        selectInput("yaxis", "Y-axis", 
+                    choices=c(
+                      "Sepal.Length", "Sepal.Width",
+                      "Petal.Length", "Petal.Width"))
       ),
       mainPanel(
         plotOutput("iris_plot")
