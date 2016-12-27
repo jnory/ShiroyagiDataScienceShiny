28393f9947d0cbe4d32b511a4d6326baf2e59ff8
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2016-12-28 02:16:14 +0900

    Change to ggplot2.

--- RegressionApp/server.R
+++ RegressionApp/server.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 shinyServer(
   function(input, output) {
     getline = reactive({
@@ -11,19 +13,26 @@ shinyServer(
       c(fun(0), fun(max(mtcars$mpg)))
     })
     output$iris_plot <- renderPlot({
-      frame()
-      cls <- mtcars$vs + 1
+      gp <- ggplot(mtcars, aes(mpg, hp))
+      gp <- gp + xlab("Miles/(US) gallon") + ylab("Gross housepower")
+      gp <- gp + theme(legend.justification = c(1, 1), legend.position=c(1, 1))
       if(input$coloring){
-        col = c("red", "blue")
+        cls <- ifelse(mtcars$vs, "V-engine", "Straight Engine")
+        gp <- gp + geom_point(aes(color=cls))
       }else{
-        col = c("black", "black")
+        gp <- gp + geom_point()
       }
-      plot(mtcars$mpg, mtcars$hp, col=col[cls], log=ifelse(input$logy, "y", ""), 
-           xlab="Miles/(US) gallon", ylab="Gross horsepower")
-      if(input$coloring){
-        legend("topleft", legend=c("V-engine", "Straight Engine"), col=col, pch="o")
+      if(input$logy){
+        gp <- gp + scale_y_log10()
       }
-      lines(c(0, max(mtcars$mpg)), getline())
+      
+      x <- rep(NA, 32)
+      x[1] <- 0
+      x[2] <- max(mtcars$mpg)
+      y <- rep(NA, 32)
+      y[1:2] <- getline()
+      df <- data.frame(x=x, y=y)
+      gp + geom_line(aes(df$x, df$y))
     })
   }
 )
