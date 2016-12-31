7a4bf73ca78229d2b904c750e5477a77a833d9b0
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2016-12-31 17:29:33 +0900

    Fix spacing.

--- GraphApp/server.R
+++ GraphApp/server.R
@@ -4,7 +4,7 @@ shinyServer(
   function(input, output) {
     output$iris_plot <- renderPlot({
       gp <- ggplot(iris, aes(Petal.Length, Petal.Width))
-      gp <- gp + theme(legend.justification = c(0, 1), legend.position=c(0, 1))
+      gp <- gp + theme(legend.justification=c(0, 1), legend.position=c(0, 1))
       if(input$coloring){
         gp + geom_point(aes(color=Species))
       }else{
